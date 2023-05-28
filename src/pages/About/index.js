c1e9940221f01ddf413b9e97b885d065c213cb5e
bear-muna <bearmuna@gmail.com>
2023-05-27 20:01:33 -0700

    outlined about me sectin

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function About() {
     return (
         <main>
-            <h1>About Page</h1>
+            <h1>About Me</h1>
+            <img src='https://placekitten.com/200/300'/>
+            <p>DESCRIPTION OF MYSELF</p>
         </main>
     )
 }
