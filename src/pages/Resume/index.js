5970d65f5c5974d9a816b0ad9b0d95d92a771066
bear-muna <bearmuna@gmail.com>
2023-05-29 13:54:24 -0700

    added proficencies to resume

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 export default function Resume() {
+    const skills = ['HTML', 'CSS', 'JavaScript', 'node.js', 'express.js', 'mySQL', 'sequelize', 'mongoDB', 'mongoose', 'React']
+    
     return (
         <main>
             <h1>Resume Page</h1>
+            <h2>Proficiencies</h2>
+            <ul>
+                {skills.map( x => <li>{x}</li>)}
+            </ul>
         </main>
     )
 }
