2c751e85f173691e0ffcc3ca8778c3b99240b30a
bear-muna <bearmuna@gmail.com>
2023-05-29 15:23:22 -0700

    added a tags to portfolio

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -4,6 +4,11 @@ export default function Portfolio() {
     return (
         <main>
             <h1>Portfolio Page</h1>
+            <a target='_blank'>WORKS 1</a>
+            <a target='_blank'>WORKS 2</a>
+            <a target='_blank'>WORKS 3</a>
+            <a target='_blank'>WORKS 4</a>
+            <a target='_blank'>WORKS 5</a>
         </main>
     )
 }
