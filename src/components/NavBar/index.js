ee39e226cf8f19a338ef8d75e9ec46d6c757a00a
bear-muna <bearmuna@gmail.com>
2023-05-30 21:33:12 -0700

    added styling to nav bar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,11 +5,13 @@ import './style.css'
 export default function NavBar() {
     return (
         <nav className='nav-bar'>
-            <h1>Welcome!</h1>
-            <Link className='nav-link' to='/'>About</Link>
-            <Link className='nav-link' to='/portfolio'>Portfolio</Link>
-            <Link className='nav-link' to='/contact'>Contact</Link>
-            <Link className='nav-link' to='/resume'>Resume</Link>
+            <h1>EJ MUNA</h1>
+            <div className='nav-links'>
+                <Link className='nav-link' to='/'>About</Link>
+                <Link className='nav-link' to='/portfolio'>Portfolio</Link>
+                <Link className='nav-link' to='/contact'>Contact</Link>
+                <Link className='nav-link' to='/resume'>Resume</Link>
+            </div>
         </nav>
     )
 }
