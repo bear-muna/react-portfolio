96c919d8a10fdd5ad8878214620a8a33591a268b
bear-muna <bearmuna@gmail.com>
2023-05-31 16:09:42 -0700

    styled message cards

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import './style.css';
 
 export default function ContactCard(props) {
     function renderCards() {
         return props.testArr.map( x => 
-        <>
-        <p>Name: {x.name}</p>
-        <p>Email: {x.email}</p>
-        <p>Message: {x.message}</p>
-        </>
+        <div className='col-lg-4 col-12 d-flex flex-column justify-content-around align-items-start p-3 m-2 message-cards'>
+            <p>Name: {x.name}</p>
+            <p>Email: {x.email}</p>
+            <p>Message: {x.message}</p>
+        </div>
     )}
 
     return (
-        <>
+        <div className='row'>
         {props.testArr ? renderCards() : null}
-        </>
+        </div>
     )
 }
