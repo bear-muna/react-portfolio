5b615d69e82c4d649940fcba475edebbee5e4e99
bear-muna <bearmuna@gmail.com>
2023-05-31 12:04:55 -0700

    started styling for contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Form from '../../components/Form';
+import './style.css';
 
 export default function Contact() {
 
     return (
-        <main>
+        <main className='container-fluid contact-page p-3'>
             <h1>Contact page</h1>
             <Form />
         </main>
