00021524327fa2502f5076bb0f5baae8aa8e3b22
bear-muna <bearmuna@gmail.com>
2023-05-30 22:04:37 -0700

    able to use images and finishing about section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './style.css'
 
 export default function About() {
     return (
-        <main>
+        <main className='about-sec'>
             <h1>About Me</h1>
-            <img src='https://placekitten.com/200/300'/>
-            <p>DESCRIPTION OF MYSELF</p>
+            <div>
+                <img src={require('../../assets/bio-quiz.PNG')}/>
+                <p>My name is Edward-John (EJ) Muna. I was formerly working in healthcare before making the leap into the creative space of a software developer. I love to stay active by either lifting, snowboarding, or practicing Brazillian Jiujitsu. Whenever I'm not moving around, I enjoy playing video games and crafting my skills in coding. This porfolio is here to highlight and showcase some of my best work as a prospecting junior software engineer. Each project has a link that you can click on and will redirect you. At the bottom of the page there are several ways to contact me, including a complete (not linked) resume. Cheers! *CURRENTY THERE ARE NO BEST WORKS OF MINE*</p>
+            </div>
         </main>
     )
 }
