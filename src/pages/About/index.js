043fdaf3c11370e961d83a44506d373ae05f0648
bear-muna <bearmuna@gmail.com>
2023-05-31 17:32:12 -0700

    media query fix for about me section

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -3,12 +3,12 @@ import './style.css'
 
 export default function About() {
     return (
-        <main className='about-sec'>
-            <h1>About Me</h1>
-            <div className='contents'>
-                <img src={require('../../assets/profile-picture.jpg')} alt='Profile Pic'/>
-                <p>My name is Edward-John (EJ) Muna. I was formerly working in healthcare before making the leap into the creative space of a software developer. I love to stay active by either lifting, snowboarding, or practicing Brazillian Jiujitsu. Whenever I'm not moving around, I enjoy playing video games and crafting my skills in coding. This porfolio is here to highlight and showcase some of my best work as a prospecting junior software engineer. Each project has a link that you can click on and will redirect you. At the bottom of the page there are several ways to contact me, including a complete (not linked) resume. Cheers! *CURRENTY THERE ARE NO BEST WORKS OF MINE*</p>
+        <div className='container-fluid p-3'>
+            <div className='row'>
+                <div className='col-12 p-2 m-2 about-me'>About Me</div>
+                <div className='col-lg-4 col-12 profile-pic p-2 m-2'></div>
+                <div className='col-lg-7 col-12 p-2 m-2'>My name is Edward-John (EJ) Muna. I was formerly working in healthcare before making the leap into the creative space of a software developer. I love to stay active by either lifting, snowboarding, or practicing Brazillian Jiujitsu. Whenever I'm not moving around, I enjoy playing video games and crafting my skills in coding. This porfolio is here to highlight and showcase some of my best work as a prospecting junior software engineer. Each project has a link that you can click on and will redirect you to its github repository. Below, you will find several ways to contact me via social media. You can also message me via form submit within the 'Contact' tab. Hope you enjoy this portfolio! Cheers!</div>
             </div>
-        </main>
+        </div>
     )
 }
