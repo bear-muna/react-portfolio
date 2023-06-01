6ff44ce1b8663e577a2efc74efa7bd71e86405a5
bear-muna <bearmuna@gmail.com>
2023-05-31 22:04:04 -0700

    fix compile error

--- src/utils/Validator/index.js
+++ src/utils/Validator/index.js
@@ -5,7 +5,7 @@ module.exports = {
         return x.match(username);
     },
     emailCheck(x) {
-        const email = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+        const email = /^[\w-.]+@([w-]+\.)+[\w-]{2,4}$/;
         return x.match(email);
     }
 }
