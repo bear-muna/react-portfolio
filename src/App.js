d7fdfbb19911754a2a7350d5c443ca8b04cbda2a
bear-muna <bearmuna@gmail.com>
2023-05-31 20:58:24 -0700

    fixed footer with svg

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ function App() {
         <Route path='/contact' element={ <Contact/> } />
         <Route path='/resume' element={ <Resume/> } />
       </Routes>
-      <hr/>
       <Footer />
     </Router>
   );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,22 @@ export default function Footer() {
     return (
         <div className='container-fluid footer-container p-3'>
             <div className='row d-flex align-items-center justify-content-center'>
-                <div className='col-1 linked-in'>Test</div>
-                <div className='col-1 github'>Test</div>
-                <div className='col-1 twitter'>Test</div>
+                <div className='col-1 text-center linked-in'>
+                    <svg width='25' height='25'>
+                        <image href={require('../../assets/linkedin-svgrepo-com.png')} height='25' width='25'/>
+                    </svg>
+                </div>
+                <div className='col-1 text-center github'>
+                    <svg width='25' height='25'>
+                        <image href={require('../../assets/github-mark-white.png')} height='25' width='25'/>
+                    </svg>
+                </div>
+                <div className='col-1 text-center twitter'>
+                    <svg width='25' height='25'>
+                        <image href={require('../../assets/iconmonstr-twitter-1-240.png')} height='25' width='25'/>
+                    </svg>
+                </div>
+
             </div>
         </div>
     )
